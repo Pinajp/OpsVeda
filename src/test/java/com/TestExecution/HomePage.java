428664934933df6c9a61ab81d5cd72616bb58fc5
Pinaj <Pinaj@192.168.1.122>
2019-06-20 12:01:33 +0530

    Error in running two classes

--- src/test/java/com/TestExecution/HomePage.java
+++ src/test/java/com/TestExecution/HomePage.java
@@ -14,11 +14,11 @@ public class HomePage extends Baseclass{
 	
 	@Test
 	public void companylogoexists() throws InterruptedException {
-		logger = report.createTest("Companylogo Exists");
+		//logger = report.createTest("Companylogo Exists");
 		driver = ic.getbrowser(driver, "Chrome");
 		wplogin = PageFactory.initElements(driver, WPLogin.class);
 		wplogin.loginsuccess(driver);
-		logger.info("Companylogo Exists");
+		//logger.info("Companylogo Exists");
 		wphome = PageFactory.initElements(driver, WPHome.class);
 		wphome.Companylogoexists(driver);
 		screen.getscreenshots(driver);
